05076e8437fc0d7edd27acaaf91482a8d641fa27
Subham95-git <roysubham505@gmail.com>
2020-09-05 21:46:09 +0530

    Updates

---
+++ precache-manifest.b89dc2b71d8915c072127b912bb40f56.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0eb8da82916f2086791d5094f8c722",
+    "url": "/Style_Revolver/index.html"
+  },
+  {
+    "revision": "a8c1fd81fdb9aa66afca",
+    "url": "/Style_Revolver/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7ddd281ef40ded085e41",
+    "url": "/Style_Revolver/static/css/main.4f23d412.chunk.css"
+  },
+  {
+    "revision": "a8c1fd81fdb9aa66afca",
+    "url": "/Style_Revolver/static/js/2.dd5dafcf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Style_Revolver/static/js/2.dd5dafcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddd281ef40ded085e41",
+    "url": "/Style_Revolver/static/js/main.93aaee30.chunk.js"
+  },
+  {
+    "revision": "57c2f94c0dc90c839b1e",
+    "url": "/Style_Revolver/static/js/runtime-main.432b9eed.js"
+  },
+  {
+    "revision": "d9296472a9949af25f0c0996ef248e09",
+    "url": "/Style_Revolver/static/media/Kurta5.d9296472.jpg"
+  },
+  {
+    "revision": "5acc7cd2b90db25f0030b3d3968f4d3c",
+    "url": "/Style_Revolver/static/media/Kurta6.5acc7cd2.jpg"
+  },
+  {
+    "revision": "73cd4a9fd6bfb91488b490a9de4f2fb1",
+    "url": "/Style_Revolver/static/media/Kurta7.73cd4a9f.jpg"
+  },
+  {
+    "revision": "a556b000a55c107ac7df78a9ee38d36c",
+    "url": "/Style_Revolver/static/media/Kurta8.a556b000.jpg"
+  },
+  {
+    "revision": "248525369e6c8d609a9b6ee742c17e2c",
+    "url": "/Style_Revolver/static/media/Men1.24852536.jpg"
+  },
+  {
+    "revision": "bf6835bc8511f067a0689debfbc9ab71",
+    "url": "/Style_Revolver/static/media/RayonKurta1.bf6835bc.jpg"
+  },
+  {
+    "revision": "3e9727a3b2b0c225845311b23dbc6a42",
+    "url": "/Style_Revolver/static/media/RayonKurta2.3e9727a3.jpg"
+  },
+  {
+    "revision": "1e4d5cd857f72ad8d14e518ff6f15b50",
+    "url": "/Style_Revolver/static/media/RayonKurta3.1e4d5cd8.jpg"
+  },
+  {
+    "revision": "d6384e77dc542b3b3cc6f223a1aa4b04",
+    "url": "/Style_Revolver/static/media/RayonKurta4.d6384e77.jpg"
+  },
+  {
+    "revision": "8275b753585d3405f1124580a66d1aee",
+    "url": "/Style_Revolver/static/media/brand.8275b753.jpg"
+  },
+  {
+    "revision": "f6ea28f6d6f70dc7a2b683434246e394",
+    "url": "/Style_Revolver/static/media/brush.f6ea28f6.jpg"
+  },
+  {
+    "revision": "e1c1235f66959e60bdf6b0a6f754aad1",
+    "url": "/Style_Revolver/static/media/brush2.e1c1235f.jpg"
+  },
+  {
+    "revision": "2d96e9343c6a3a1118fd327ed2336c59",
+    "url": "/Style_Revolver/static/media/brush3.2d96e934.jpg"
+  },
+  {
+    "revision": "a379f89d1a93c288ae86a4ae5c620bd2",
+    "url": "/Style_Revolver/static/media/brush4.a379f89d.jpg"
+  },
+  {
+    "revision": "54c26015874405d5a334750804502cdb",
+    "url": "/Style_Revolver/static/media/handloom.54c26015.jpg"
+  },
+  {
+    "revision": "5a646c44abed69c2f288072c24fe7938",
+    "url": "/Style_Revolver/static/media/kurta.5a646c44.jpg"
+  },
+  {
+    "revision": "67788d42835f0eedb0ebc2abe474442e",
+    "url": "/Style_Revolver/static/media/kurta2.67788d42.jpg"
+  },
+  {
+    "revision": "05f595cd3f8af6fa36e33fe2f294cbc9",
+    "url": "/Style_Revolver/static/media/kurta3.05f595cd.jpg"
+  },
+  {
+    "revision": "ed92b58f71e179924e2245ac0e7ee4c5",
+    "url": "/Style_Revolver/static/media/kurta4.ed92b58f.jpg"
+  },
+  {
+    "revision": "650fde331b51b9e29ab82838a6d2efab",
+    "url": "/Style_Revolver/static/media/mentshirt.650fde33.jpg"
+  },
+  {
+    "revision": "b8a3f5525f73a70aee5f3a086add73d3",
+    "url": "/Style_Revolver/static/media/mentshirt2.b8a3f552.jpg"
+  },
+  {
+    "revision": "8a2e0925eb72ad31e398d7328c903cbb",
+    "url": "/Style_Revolver/static/media/mentshirt3.8a2e0925.jpg"
+  },
+  {
+    "revision": "80985aa8211f9f125483d7d118ee2d31",
+    "url": "/Style_Revolver/static/media/nailPolish.80985aa8.jpg"
+  },
+  {
+    "revision": "92caee5ac8c6c36363ef5efdf59e7c45",
+    "url": "/Style_Revolver/static/media/nailPolish2.92caee5a.jpg"
+  },
+  {
+    "revision": "5a3dd5ca51a1d0edaadf54dc478289d9",
+    "url": "/Style_Revolver/static/media/nailPolish3.5a3dd5ca.jpg"
+  },
+  {
+    "revision": "bbba09e81c74119a0894c2cc741182c5",
+    "url": "/Style_Revolver/static/media/nailPolish4.bbba09e8.jpg"
+  },
+  {
+    "revision": "341fea96b0457c93b49cd3eb110fc8a6",
+    "url": "/Style_Revolver/static/media/sale.341fea96.jpg"
+  },
+  {
+    "revision": "5fc7dccfdc5be9d53a142f0e3fac7ee1",
+    "url": "/Style_Revolver/static/media/sale2.5fc7dccf.jpg"
+  },
+  {
+    "revision": "64690d38ec231680266d3f1a1395788c",
+    "url": "/Style_Revolver/static/media/saree.64690d38.jpg"
+  },
+  {
+    "revision": "60f090668e4abeb82f1a003d994cd5e9",
+    "url": "/Style_Revolver/static/media/women4.60f09066.jpg"
+  },
+  {
+    "revision": "72b1d4ff8520e903a25184079078bbab",
+    "url": "/Style_Revolver/static/media/women5.72b1d4ff.jpg"
+  }
+]);
